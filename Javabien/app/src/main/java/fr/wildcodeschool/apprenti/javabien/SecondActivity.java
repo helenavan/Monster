94a88e033dfb55d3742ea9d11ef0fe6a37cdc175
helenavan <tedone.bd@gmail.com>
2016-09-21 19:03:56 +0200

    clonage

--- Javabien/app/src/main/java/fr/wildcodeschool/apprenti/javabien/SecondActivity.java
+++ Javabien/app/src/main/java/fr/wildcodeschool/apprenti/javabien/SecondActivity.java
@@ -2,10 +2,6 @@ package fr.wildcodeschool.apprenti.javabien;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.widget.AdapterView;
-import android.widget.GridView;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -13,17 +9,5 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
-
-        GridView gridView = (GridView) findViewById(R.id.gridview);
-        gridView.setAdapter(new ImageAdapter(this));
-
-
-
     }
-    /**@Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }**/
 }
